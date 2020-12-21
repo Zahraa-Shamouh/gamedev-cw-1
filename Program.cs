c80336897793d663c2ee9cb00ca812a0da27616e
Zahraa-shamouh <zahraa.m.shamouh@gmail.com>
2020-12-21 16:10:25 +0300

    gamedev-1

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace CW1
             int Age = 15;
             float speed = 1.65f;
 
-            Console.WriteLine("My name is {0}, my super powers are {1} and {2}, my age is {3} and my speed is {4}", characterName, SuperPower1, SuperPower2, Age, speed);
+            Console.WriteLine("My name is {0}, my super powers are {1} and {2}. My age is {3} and my speed is {4}", characterName, SuperPower1, SuperPower2, Age, speed);
         }
     }
 }
