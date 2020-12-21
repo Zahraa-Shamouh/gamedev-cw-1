7a657eff9ad929d6df8997a482f0d5c1d922303a
Zahraa-shamouh <zahraa.m.shamouh@gmail.com>
2020-12-21 19:44:59 +0300

    final

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -4,8 +4,9 @@ namespace ConsoleApp2
 {
     class Program
     {
+        static double playerSpeed;
         static void Main(string[] args)
-        {
+        { 
             string characterName = "Ahmad";
             string SuperPower1 = "Steel Shiled";
             string SuperPower2 = "Night Vision";
@@ -18,16 +19,44 @@ namespace ConsoleApp2
             string HeroSuperPower1 = "Steel Shiled";
             string HeroSuperPower2 = "Night Vision";
 
-            Console.WriteLine("My name is {0}, my super powers are {1} and {2}. my age is {3} and my speed is {4}", characterName, SuperPower1, SuperPower2, Age, speed);
-            Console.WriteLine("My name is {0}, my super powers are {1} and {2}. my age is {3} and my hight is {4}", HeroName, HeroSuperPower1, HeroSuperPower2, HeroAge, HeroHeight);
+            Console.WriteLine("My name is {0}, my super powers are {1} and {2}. My age is {3} and my speed is {4}", characterName, SuperPower1, SuperPower2, Age, speed);
+            Console.WriteLine("My name is {0}, my super powers are {1} and {2}. My age is {3} and my hight is {4}", HeroName, HeroSuperPower1, HeroSuperPower2, HeroAge, HeroHeight);
+          
             int AgeDifference = HeroAge - Age;
             Console.WriteLine ("The Age difference is {0}", AgeDifference);
 
-
+            //Bonus
             if (SuperPower1 == HeroSuperPower1)
             {
                 Console.WriteLine("{0} and {1} has the same super power", characterName, HeroName);
             }
+
+            SetSpped(2.5);
+            Console.WriteLine("The current player speed is: " + GetSpeed());
+            characterPower(); 
+         
+        }//end main. 
+
+        static void SetSpped(double NewSpeed)
+        {
+            playerSpeed = NewSpeed; 
         }
+
+        static double GetSpeed()
+        {
+            return playerSpeed; 
+        }
+
+        static void characterPower ()
+        {
+            int[] characterPowerValue = { 100, 200, 390, 300, 290 };
+            if (characterPowerValue[0] > characterPowerValue[2])
+                Console.WriteLine(" character power value 1 is greater"); 
+            else if (characterPowerValue[0] < characterPowerValue[2])
+                Console.WriteLine(" character power value 3 is greater");
+            else
+                Console.WriteLine(" character power value 1 and 3 are equal");
+        }
+
     }
 }
