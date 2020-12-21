aa6c5f0b1a83e5d731ef90f4d25f829337d21b59
Zahraa-shamouh <zahraa.m.shamouh@gmail.com>
2020-12-21 16:49:11 +0300

    profram.cs

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "Ahmad";
+            string SuperPower1 = "Steel Shiled";
+            string SuperPower2 = "Night Vision";
+            int Age = 15;
+            float speed = 1.65f;
+
+            Console.WriteLine("My name is {0}, my super powers are {1} and {2}. my age is {3} and my speed is {4}", characterName, SuperPower1, SuperPower2, Age, speed);
+        }
+    }
+}
